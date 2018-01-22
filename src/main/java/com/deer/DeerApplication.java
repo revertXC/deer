74f82770e7110795ada0aa274d5ce60837989492
xcshuai <1376477341@qq.com>
2018-01-22 22:32:24 +0800

    第一次提交

---
+++ src/main/java/com/deer/DeerApplication.java
@@ -0,0 +1,26 @@
+package com.deer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication(scanBasePackages = "com.deer")
+public class DeerApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(DeerApplication.class,args);
+    }
+    /**
+     *新增此方法
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(DeerApplication.class);
+    }
+
+
+    public void aaa(){
+        System.out.println();
+    }
+}
